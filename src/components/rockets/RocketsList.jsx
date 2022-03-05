3d83d93d88ae75cda5e4b5069012cfd5f509f6f8
tYohannes <teklegy@gmail.com>
2022-03-05 21:43:51 +0300

    Fetch and render rockets

--- src/components/rockets/RocketsList.jsx
+++ src/components/rockets/RocketsList.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-const RocketsList = () => (
-  <div>RocketsList</div>
-);
+import { fetchRockets } from '../../store/rockets';
+
+const RocketsList = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state);
+  useEffect(async () => {
+    dispatch(fetchRockets());
+    console.log(rockets);
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {
+          rockets.map((rocket) => (
+            <div key={rocket.id}>
+              <img src={rocket.rocketImage} alt="rocket" />
+              <h2>{rocket.rocketName}</h2>
+              <p>{rocket.description}</p>
+            </div>
+          ))
+        }
+      </ul>
+    </div>
+  );
+};
 
 export default RocketsList;
